371203fed5fd3167b6a92a3936aa6d9adeb7dd07
ctames <cta5bb@virginia.edu>
2014-06-03 14:19:02 -0400

    update nightly, ~[] to Vec, a little progress

--- tutorialmapreduce.rs
+++ tutorialmapreduce.rs
@@ -1,24 +1,47 @@
 use std::fmt::Show; 
 
 fn main() {
-	let mut words: ~[String] = ~["these".to_string(), 
-		"are".to_string(), "words".to_string()];
 	
-	fn create_pair(s: &String) -> (String, int) {
-		(s.clone(), 1)
+	let mut strings: Vec<String> = vec!("these are words".to_string(), 
+		"those are words".to_string(), 
+		"lots of words".to_string());
+	
+	// function for map
+	fn create_pairs(s: &String) -> Vec<(String, int)> {
+		let mut retvals: Vec<(String,int)> = vec!(); 
+		for word in s.as_slice().split(' ') {
+			println!("{}", word);
+			retvals.push((word.to_string(), 1));
+		}
+		retvals
 	}
 	
-	words.mapr(create_pair);	
+	// function for reduce
+	fn reduce_pairs(pairs: Vec<(String, int)>) -> Vec<(String, int)> {
+		vec!(("".to_string(), 1))
+	}
+	
+	// let's do it
+	strings.mapreduce::<String,int>(create_pairs, reduce_pairs);	
 }
 
-trait Map {
-	fn mapr<K: Show, V: Show>(&mut self, fn(&String) -> (K, V));
+trait MapReduce {
+	fn mapreduce<K: Show, V: Show>(&mut self, fn(&String) -> Vec<(K, V)>, 
+		fn(Vec<(K, V)>) -> Vec<(K, V)>);
 }
 
-impl Map for ~[String] {
-	fn mapr<K: Show, V: Show>(&mut self, f: fn(&String) -> (K,V)) {
+impl MapReduce for Vec<String> {
+	fn mapreduce<K: Show, V: Show>(&mut self, mapf: fn(&String) -> Vec<(K, V)>, 
+		redf: fn(Vec<(K, V)>) -> Vec<(K, V)>) {
+		
+		let mut values: Vec<(K, V)> = vec!();
 		for item in (*self).iter() {
-			println!("{}", f(item)); 
+			values = mapf(item);
+			for pair in values.iter() {
+				println!("{}", pair); 
+			} 
 		}
+		
+		// then reduce
 	}
 }	
